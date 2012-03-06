a555c43387be75e6133b11f57a85b2240be3de75
Shea Levy <shea@shealevy.com>
2012-03-06 17:24:21 -0500

    Set up join model association

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,4 @@
 class Invoice < ActiveRecord::Base
+  has_many :invoices_time_logss
+  has_many :time_logs, through: :invoices_time_logss
 end
--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -1,2 +1,4 @@
 class TimeLog < ActiveRecord::Base
+  has_one :invoices_time_logs
+  has_one :invoice, through: :invoices_time_logs
 end
